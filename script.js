5ccb1dfac5dfb5bd7434f33dc0077141745b4b1a
AmirAlEzzani <102375974+AmirAlEzzani@users.noreply.github.com>
2023-08-30 12:55:20 -0700

    landing

--- script.js
+++
@@ -1,7 +0,0 @@
-const toggleButton = document.getElementById("toggleButton");
-const collapsibleContainer = document.getElementById("collapsibleContainer");
-
-toggleButton.addEventListener("click", () => {
-  collapsibleContainer.classList.toggle("open");
-  toggleButton.classList.toggle("flip"); // Toggle the "flip" class
-});
